ec9e399071416f811d27330e689f2d699fa7d9c2
developernilesh <nileshmukherjee2017@gmail.com>
2025-08-06 23:54:59 +0530

    Added LandingPage component to integrate all sections and layout elements

---
+++ src/components/pages/LandingPage.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { HeroSection } from "@/components/sections/HeroSection";
+import { FeaturesSection } from "@/components/sections/FeaturesSection";
+import { HowItWorksSection } from "@/components/sections/HowItWorksSection";
+import { SecuritySection } from "@/components/sections/SecuritySection";
+import { PricingSection } from "@/components/sections/PricingSection";
+import { CTASection } from "@/components/sections/CTASection";
+import { Footer } from "@/components/layout/Footer";
+import { Navbar } from "@/components/layout/Navbar";
+
+export function LandingPage() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <HeroSection />
+        <FeaturesSection />
+        <HowItWorksSection />
+        <SecuritySection />
+        <PricingSection />
+        <CTASection />
+      </main>
+      <Footer />
+    </>
+  );
+} 
