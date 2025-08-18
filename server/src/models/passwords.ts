9c9e436cedce29e56cceb186385eaf6b314d014d
developernilesh <nileshmukherjee2017@gmail.com>
2025-08-18 23:59:35 +0530

    Refactored Passwords model to define schema and export model

--- server/src/models/passwords.ts
+++ server/src/models/passwords.ts
@@ -1,1 +1,15 @@
-import mongoose from "mongoose"
+import { model, Schema } from "mongoose";
+
+const passwordSchema = new Schema(
+  {
+    userid: String,
+    title: String,
+    url: String,
+    username: String,
+    password: String,
+    category: String,
+  },
+  { timestamps: true }
+);
+
+export const PasswordsModel = model("Passwords", passwordSchema)
