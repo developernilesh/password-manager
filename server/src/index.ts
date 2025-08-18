7fbbca899addaf5604f33f58a704778cafc320bb
developernilesh <nileshmukherjee2017@gmail.com>
2025-08-18 23:58:56 +0530

    Setted up Express server with database connection and initial routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,21 @@
 import express from "express";
+import connectDB from "./config/database.js";
+import { addPassword } from "./controllers/passwords.js";
+
 const app = express();
+connectDB();
+
+app.use(express.json());
+
+app.post("/api/v1/add-password", addPassword);
+
+
+app.get("/", (req, res) => {
+  return res.json({
+    success: true,
+    message: "Your server is up and running....",
+  });
+});
 
 app.listen(4000, () => {
   console.log("Server Started at Port 4000");
