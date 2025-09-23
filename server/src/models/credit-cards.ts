2e020b71439f23910e6d7b2086549df855a141a5
developernilesh <nileshmukherjee2017@gmail.com>
2025-09-23 23:46:21 +0530

    Structured the schema for the credit cards

--- server/src/models/credit-cards.ts
+++ server/src/models/credit-cards.ts
@@ -2,16 +2,91 @@ import { model, Schema } from "mongoose";
 
 const creditCardSchema = new Schema(
   {
-    userid: String,
-    cardName: String,
-    bank: String,
-    category: String,
-    cardholderName: String,
-    expiry: String,
-    cardNumber: String,
-    cvv: String,
+    userid: {
+      type: String,
+      required: true,
+    },
+    cardName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    bank: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    cardholderName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    expiry: {
+      type: String,
+      required: true,
+    },
+    encryptedCardNumber: {
+      type: String,
+      required: true,
+    },
+    cardNoEncryptionParams: {
+      iv: {
+        type: String,
+        required: true,
+      },
+      salt: {
+        type: String,
+        required: true,
+      },
+      hmac: {
+        type: String,
+        required: true,
+      },
+      algorithm: {
+        type: String,
+        required: true,
+        default: "AES-256-CBC",
+      },
+      version: {
+        type: String,
+        required: true,
+        default: "1.0",
+      },
+    },
+    encryptedCvv: {
+      type: String,
+      required: true,
+    },
+    CvvEncryptionParams: {
+      iv: {
+        type: String,
+        required: true,
+      },
+      salt: {
+        type: String,
+        required: true,
+      },
+      hmac: {
+        type: String,
+        required: true,
+      },
+      algorithm: {
+        type: String,
+        required: true,
+        default: "AES-256-CBC",
+      },
+      version: {
+        type: String,
+        required: true,
+        default: "1.0",
+      },
+    },
   },
   { timestamps: true }
 );
 
-export const CreditCardsModel = model("CreditCards", creditCardSchema)
+export const CreditCardsModel = model("CreditCards", creditCardSchema);
