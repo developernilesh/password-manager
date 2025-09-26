71dfe1e9d17082dbe7a8121970dbfa7ba8fede9f
developernilesh <nileshmukherjee2017@gmail.com>
2025-09-26 23:57:47 +0530

    Functionalities for updating credit card and delete a credit card are added

--- server/src/controllers/credit-cards.ts
+++ server/src/controllers/credit-cards.ts
@@ -1,6 +1,3 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 import type { Request, Response } from "express";
 import { CreditCardsModel } from "../models/credit-cards.js";
 
@@ -61,6 +58,147 @@ export const addCreditCard = async (req: Request, res: Response) => {
       success: true,
       message: "Card added successfully!",
       newCard,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: `Something went wrong: ${
+        (error as { message: string }).message
+      }`,
+    });
+  }
+};
+
+export const editCreditCard = async (req: Request, res: Response) => {
+  try {
+    const { cardId } = req.params;
+
+    const {
+      userid,
+      cardName,
+      bank,
+      category,
+      cardholderName,
+      expiry,
+      encryptedCardNumber,
+      cardNoEncryptionParams,
+      encryptedCvv,
+      cvvEncryptionParams,
+    } = req.body;
+
+    if (
+      !cardId ||
+      !userid ||
+      !cardName ||
+      !bank ||
+      !category ||
+      !cardholderName ||
+      !expiry ||
+      !encryptedCardNumber ||
+      !cardNoEncryptionParams ||
+      !encryptedCvv ||
+      !cvvEncryptionParams
+    ) {
+      return res.status(400).json({
+        success: false,
+        message: "Cannot update card info at moment. Please try again!",
+      });
+    }
+
+    const cardToEdit = await CreditCardsModel.findById(cardId);
+
+    if (!cardToEdit) {
+      return res.status(400).json({
+        success: false,
+        message: "Unable to fetch data. Please try again!",
+      });
+    }
+
+    if (cardToEdit.userid !== userid) {
+      return res.status(400).json({
+        success: false,
+        message: "You are not authenticated to update this card!",
+      });
+    }
+
+    cardToEdit.cardName = cardName;
+    cardToEdit.bank = bank;
+    cardToEdit.cardholderName = cardholderName;
+    cardToEdit.category = category;
+    cardToEdit.expiry = expiry;
+    cardToEdit.encryptedCardNumber = encryptedCardNumber;
+    cardToEdit.cardNoEncryptionParams = cardNoEncryptionParams;
+    cardToEdit.encryptedCvv = encryptedCvv;
+    cardToEdit.cvvEncryptionParams = cvvEncryptionParams;
+
+    const updatedCardInfo = await cardToEdit.save();
+
+    if (!updatedCardInfo) {
+      return res.status(400).json({
+        success: false,
+        message: "Cannot update card info at moment. Please try again!",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Card info updated successfully!",
+      updatedCardInfo,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: `Something went wrong: ${
+        (error as { message: string }).message
+      }`,
+    });
+  }
+};
+
+export const deleteCreditCard = async (req: Request, res: Response) => {
+  try {
+    const { cardId } = req.params;
+
+    const { userid } = req.body;
+
+    if (!cardId || !userid) {
+      return res.status(400).json({
+        success: false,
+        message: "Cannot delete card at moment. Please try again!",
+      });
+    }
+
+    const cardToDelete = await CreditCardsModel.findById(cardId);
+
+    if (!cardToDelete) {
+      return res.status(400).json({
+        success: false,
+        message: "Unable to fetch data. Please try again!",
+      });
+    }
+
+    if (cardToDelete.userid !== userid) {
+      return res.status(400).json({
+        success: false,
+        message: "You are not authenticated to update this card!",
+      });
+    }
+
+    const cardsAfterDeleteion = await CreditCardsModel.findByIdAndDelete(
+      cardId
+    );
+
+    if (!cardsAfterDeleteion) {
+      return res.status(400).json({
+        success: false,
+        message: "Cannot delete card at moment. Please try again!",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Card deleted successfully!",
+      cardsAfterDeleteion,
     });
   } catch (error) {
     res.status(500).json({
